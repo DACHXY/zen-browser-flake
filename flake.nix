f8c571bbd21dadbfcb8148b2c03b424ceef553d6
danny <danny10132024@gmail.com>
2025-11-29 15:40:31 +0800

    fix: add binary path to update script

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
       packages."${system}" = rec {
         zen-browser = pkgs.callPackage ./nix/package.nix { inherit (source) src version; };
         update = pkgs.writeShellScriptBin "fetch-source" ''
+          PATH="$PATH:${pkgs.jq}/bin:${pkgs.curl}/bin"
           VERSION="$(curl -s https://api.github.com/repos/zen-browser/desktop/releases/latest | jq -r .tag_name)"
           URL="https://github.com/zen-browser/desktop/releases/download/$VERSION/zen.linux-x86_64.tar.xz"
           HASH="$(nix-prefetch-url --unpack $URL)"
